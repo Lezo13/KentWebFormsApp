834f9d12cfe96cbf0326ce2417b07b0a42b6ae8f
Kent Laurence Gentilezo <kgentilezo@you-source.com>
2023-06-08 19:23:09 +0800

    Added role seed on db migration

---
+++ KentWebForms.App/Migrations/Configuration.cs
@@ -0,0 +1,35 @@
+﻿namespace KentWebForms.App.Migrations
+{
+    using KentWebForms.App.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "ApplicationDbContext";
+        }
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole("Admin");
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("User"))
+            {
+                var role = new IdentityRole("User");
+                roleManager.Create(role);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
