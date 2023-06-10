e056ee97255f678e4b26518c2b21786977e30e1f
Kent Laurence Gentilezo <kgentilezo@you-source.com>
2023-06-11 01:58:41 +0800

    Setup dependency injection

---
+++ KentWebForms.App/App_Start/UnityConfig.cs
@@ -0,0 +1,29 @@
+namespace KentWebForms.App
+{
+    using System;
+    using Unity;
+    using KentWebForms.App.Injection;
+
+    public static class UnityConfig
+    {
+        #region Unity Container
+        private static Lazy<IUnityContainer> container =
+          new Lazy<IUnityContainer>(() =>
+          {
+              var container = new UnityContainer();
+              RegisterTypes(container);
+              return container;
+          });
+
+        /// <summary>
+        /// Configured Unity Container.
+        /// </summary>
+        public static IUnityContainer Container => container.Value;
+        #endregion
+
+        public static void RegisterTypes(IUnityContainer container)
+        {
+            Infrastructure.InjectInfrastructure(container);
+        }
+    }
+}
